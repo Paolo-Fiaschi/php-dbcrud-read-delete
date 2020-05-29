08b9dbd8bbaba2a0c79390e33411dc24f1c5d570
Paolo <p.fiaschi@hotmail.it>
2020-05-29 15:06:21 +0200

    delete pagante

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-
+function deletePagante() {
+    var cestinoIcon = $(this);
+    var paganteHtml = cestinoIcon.parent();
+    var id = paganteHtml.data('id');
+    console.log(id);
+    $.ajax({
+        url: "deletepaganti.php",
+        method: "POST",
+        data:{
+            id: id
+        },
+        success: function () {
+            paganteHtml.remove();
+        },error: function (err) {
+            console.error(err);
+        }
+    });
+}
+$("#target").on("click", ".delete", deletePagante);
 function printPaganti(paganti) {
     var source = $("#pagante-template").html();
     var template = Handlebars.compile(source);
