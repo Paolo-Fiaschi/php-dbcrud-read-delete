86045d9f3f249d05a3b2bd7cbb0e4f7b69740f93
Paolo <p.fiaschi@hotmail.it>
2020-05-29 14:21:04 +0200

    get paganti data php

---
+++ getPaganti.php
@@ -0,0 +1,33 @@
+<?php
+    header('Content-Type: application/json');
+    $server = 'localhost';
+    $username = 'root';
+    $password = 'root';
+    $dbName = 'HotelDb';
+
+    $conn = new mysqli($server, $username, $password, $dbName);
+
+    if ($conn -> connect_errno) {
+        echo $conn -> connect_errno;
+        return;
+    }
+
+    $sql = "
+        SELECT id, name, lastname, address
+        FROM paganti
+    ";
+
+    $results = $conn -> query($sql);
+    if ($results -> num_rows < 1) {
+        echo "no results";
+        return;
+    }
+    $res = [];
+
+    while ($row = $results -> fetch_assoc()) {
+        $res [] = $row;
+    }
+    $conn -> close();
+
+    echo json_encode($res);
+
